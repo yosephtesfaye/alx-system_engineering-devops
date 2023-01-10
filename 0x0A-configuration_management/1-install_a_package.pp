3757a1de77d789e77dcf19449904370e8fce7890
yosephtesfaye <tewdemam@gmail.com>
2023-01-10 05:48:25 -0800

    modified

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+# This Puppet manifest installs puppet-lint
+
+package { 'puppet-lint':
+
+  ensure   => '2.1.1',
+
+  provider => 'gem',
+
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,11 @@
+# This Puppet manifest kills a process named killmenow
+
+
+
+exec { 'process kill killmenow':
+
+  path    => '/usr/bin/',
+
+  command => 'pkill -f killmenow',
+
+}
